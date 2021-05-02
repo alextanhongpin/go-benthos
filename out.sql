7567966fa92722286b68cb86bb95782ac9ef112c
alextanhongpin <alextan220990@gmail.com>
2021-05-02 15:56:14 +0800

    chore: update example, add unit test

--- out.sql
+++ out.sql
@@ -1,2 +1,8 @@
 INSERT INTO users(name) VALUES ('first foo');
 INSERT INTO users(name) VALUES ('second foo');
+INSERT INTO users(name) VALUES ('first foo');
+INSERT INTO users(name) VALUES ('second foo');
+{"bar":"third bar","baz":"third baz","foo":""}
+INSERT INTO users(name) VALUES ('first foo');
+INSERT INTO users(name) VALUES ('second foo');
+{"bar":"third bar","baz":"third baz","foo":""}
